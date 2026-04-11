0fad2e5d2a4a5c9da7430ab25903de43dad99926
vadzim <vadzim@vadzim>
2026-04-11 23:19:02 +0200

    formatting

--- src/tests/type-test-utils.test.ts
+++ src/tests/type-test-utils.test.ts
@@ -1,8 +1,9 @@
 import { describe, it } from "node:test"
 import type { Expect, Matches } from "../test-utils/type-test-utils.js"
 
-export type ExpectDoesntMatch<T extends false> = T
+type ExpectDoesntMatch<T extends false> = T
 
+// check that Matches does not accept `any` type
 type _MatchesAnyLeftIsFalse = ExpectDoesntMatch<Matches<any, string>>
 type _MatchesAnyRightIsFalse = ExpectDoesntMatch<Matches<string, any>>
 type _MatchesAnyAnyIsFalse = ExpectDoesntMatch<Matches<any, any>>
