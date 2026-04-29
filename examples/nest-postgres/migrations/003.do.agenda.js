743181857ae7dd104ca66007eea71a3b227d88db
vadzim <vadzim@vadzim>
2026-04-29 18:51:32 +0200

    formatting

--- examples/nest-postgres/migrations/003.do.agenda.js
+++ examples/nest-postgres/migrations/003.do.agenda.js
@@ -5,10 +5,8 @@ export const generateSql = () =>
   create table if not exists public.agenda (
     id uuid not null,
     created_at timestamp with time zone not null,
-    meeting_id uuid,
     user_id uuid not null,
-    workspace_id uuid,
-    agenda_blocks jsonb,
-    agenda_raw text
+    title text not null,
+    agenda text
   );
 `)
