b1bd3e8c54809658f961520fba2578454e22af3e
vadzim <vadzim@vadzim>
2026-05-08 23:21:56 +0200

    Migrate parse-drop-schema.ts to FormatError

--- test/parse-drop-schema.test.ts
+++ test/parse-drop-schema.test.ts
@@ -1,7 +1,7 @@
 import { describe, it } from "node:test"
 import type { JsqlSchemaShape } from "../src/core/jsql-shapes.ts"
 import type { ParseSqlTokens } from "../src/lexer/sql-tokens.ts"
-import type { SqlParserError } from "../src/sql-parser-error.ts"
+import type { DbtyperError } from "../src/sql-parser-error.ts"
 import type { Expect, Extends, Matches } from "./test-utils/type-test-utils.ts"
 import type { ParseSqlStatement } from "../src/parser/parse-sql-statement.ts"
 
@@ -38,19 +38,19 @@ type _d2null = Expect<Matches<D2[2], null>>
 type _d2db = Expect<Matches<D2[1], DbWithAuth>>
 
 type D3 = ParseSqlStatement<ParseSqlTokens<`drop schema ghost;`>, DbWithAuth>
-type _d3err = Expect<Matches<D3[2], SqlParserError<"Schema does not exist; use IF EXISTS">>>
+type _d3err = Expect<Matches<D3[2], DbtyperError<3201, "Schema does not exist; use IF EXISTS">>>
 
 type D4 = ParseSqlStatement<ParseSqlTokens<`drop schema auth;`>, DbMulti>
 type _d4shape = Expect<Matches<D4[1], DbMultiDroppedAuth>>
 
 type DMissingSemi = ParseSqlStatement<ParseSqlTokens<`drop schema auth trailing`>, DbWithAuth>
-type _dMissingSemi = Expect<Extends<DMissingSemi[2], SqlParserError<"Expected `;` after DROP SCHEMA">>>
+type _dMissingSemi = Expect<Extends<DMissingSemi[2], DbtyperError<3801, "Expected `;` after DROP SCHEMA">>>
 
 type DMissingName = ParseSqlStatement<ParseSqlTokens<`drop schema ;`>, DbWithAuth>
-type _dMissingName = Expect<Extends<DMissingName[2], SqlParserError<"Expected schema name in DROP SCHEMA">>>
+type _dMissingName = Expect<Extends<DMissingName[2], DbtyperError<3800, "Expected schema name in DROP SCHEMA">>>
 
 type DIfWrong = ParseSqlStatement<ParseSqlTokens<`drop schema if not exists auth;`>, DbWithAuth>
-type _dIfWrong = Expect<Extends<DIfWrong[2], SqlParserError<"Expected `exists` after `IF` in DROP SCHEMA">>>
+type _dIfWrong = Expect<Extends<DIfWrong[2], DbtyperError<3802, "Expected `exists` after `IF` in DROP SCHEMA">>>
 
 describe("parse-drop-schema (type tests)", () => {
 	it("compile-time assertions above", () => {})
