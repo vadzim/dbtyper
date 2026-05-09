167521b91ce1417950177da73b1b0a4c4081c628
vadzim <vadzim@vadzim>
2026-05-09 09:47:52 +0200

    Move WHERE error tests to integration tests (batch 1/3)
    - Migrated 12 error tests from parse-where-expression.test.ts
    - Created test/integration/where/ directory
    - Tests cover: unknown columns, unbalanced parens, IN errors, IS errors
    - All tests use DbtyperError format with error codes

---
+++ test/integration/where/where-unbalanced-paren-in-list.error.test.ts
@@ -0,0 +1,21 @@
+// Integration Test: WHERE - Missing closing paren in IN list
+import type { DbtyperError } from "../../../src/sql-parser-error.ts"
+import type { ExtractQueryError } from "../../../src/core/sql-query.ts"
+import type { Expect, Matches } from "../../test-utils/type-test-utils.ts"
+import { mockDriver } from "../../test-utils/test-databases.ts"
+
+const db = mockDriver.database()
+	.schema("public", s => s
+		.table("users", t => t
+			.column("id", c => c.text())
+			.column("name", c => c.text())))
+
+const query = `SELECT * FROM users WHERE users.id in ( 'u'` as const
+
+// @ts-expect-error
+db.query(query)
+
+type _errorCheck = Expect<Matches<
+	ExtractQueryError<typeof db, typeof query>,
+	DbtyperError<-1, string>
+>>
